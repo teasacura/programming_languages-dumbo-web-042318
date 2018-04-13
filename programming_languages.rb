5a9377c825047c11107d4400ffc03e8ea266dde6
teasacura <teasacuraster@gmail.com>
2018-04-13 15:20:53 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, list|
+    list.each do |name, description|
+      new_hash[name] = description
+    end
+  end
+
+  new_hash.each do |name, description|
+    if languages[:oo].keys.include?(name) && languages[:functional].keys.include?(name)
+      new_hash[name][:style] = [:oo, :functional]
+    elsif languages[:oo].keys.include?(name)
+      new_hash[name][:style] = [:oo]
+    else
+      new_hash[name][:style] = [:functional]
+    end
+  end
+
+  new_hash
 end
+
+# description.each do |info, data|
+#   if name == :ruby
+#   n_hash = {style:[type]}
+#   new_hash[name] = description.merge(n_hash)
+#   end
+# end
